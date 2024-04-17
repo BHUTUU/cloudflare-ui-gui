04cf3fec39abe6ff5e299bc2469d7f03554f4f9a
BHUTUU <raj259942@gmail.com>
2024-04-17 22:34:21 +0530

    Revert "Update cloudflare.py"
    This reverts commit 410860301e9d82864e0d95e9b8b5788cc9a93b29.

--- cloudflare.py
+++ cloudflare.py
@@ -57,8 +57,7 @@ def getCache():
 #<<<--------Function-------->>>
 def cloudflare(lhost, lport):
     if realName == 'windows':
-        os.chdir(gitDir)
-        cloudflare_command = './cloudflared -url '+str(lhost)+':'+str(lport)+' --logfile '+cloudflare_log+' >'+os.devnull+' 2>&1'
+        cloudflare_command = 'cloudflared -url '+str(lhost)+':'+str(lport)+' --logfile '+cloudflare_log+' >'+os.devnull+' 2>&1'
         # subprocess.Popen(cloudflare_command)
         with open(os.devnull, 'wb') as log:
             running = subprocess.Popen(cloudflare_command, stdout=log, stderr=log, shell=True)
